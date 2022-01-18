0f79e5f0ddcf93e0de97901c946044c388a2e39e
Benjamin144 <josephroberts@email.com>
2022-01-18 20:11:33 +0000

    initial commit

---
+++ base-project/jewels.py
@@ -0,0 +1,7 @@
+print("**************************************")
+password = input("Enter password: ")
+if password == "qwerty":
+    print("Correct password!")
+else:
+    print("Incorrect password")
+print("**************************************")
