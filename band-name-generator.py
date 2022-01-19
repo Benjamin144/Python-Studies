46444368261683a0facc26535f377c832507c882
Benjamin144 <josephroberts@email.com>
2022-01-19 20:28:43 +0000

    Name Generator app

---
+++ band-name-generator.py
@@ -0,0 +1,4 @@
+print('Welcome to my Band Name Generator\n')
+town = input ('What Town, City or Village did you grow up in?\n')
+pet = input ('What is the name of your pet?\n')
+print ('Your band name could be ' + town, pet )
