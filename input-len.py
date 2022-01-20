e86bcdea2270577ba8735c93f6db2e32d7c676e8
Benjamin144 <josephroberts@email.com>
2022-01-20 07:48:13 +0000

    adding extra variables for later use

--- input-len.py
+++ input-len.py
@@ -2,5 +2,10 @@
 # print(len(name))
 
 # Cleaner alternative method
+# print(len(input("What is your name? ")))
 
-print(len(input("What is your name? ")))
+#elaboration on original method using an extra variable
+
+name = input("What is your name? ")
+length = len(name)
+print(length)
