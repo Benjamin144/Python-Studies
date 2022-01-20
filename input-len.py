e8112be7c926d20066ee516b4ec67575e1bc65f9
Benjamin144 <josephroberts@email.com>
2022-01-20 07:34:41 +0000

    cleaner method for calculating length of string

--- input-len.py
+++ input-len.py
@@ -1,2 +1,6 @@
-name = input("What is your name?" + " ")
-print(len(name))
+# name = input("What is your name?" + " ")
+# print(len(name))
+
+# Cleaner alternative method
+
+print(len(input("What is your name? ")))
