d5e273ed8ae44910fa0f7cfe2c77f4b19374cc48
Benjamin144 <josephroberts@email.com>
2022-02-08 22:47:23 +0000

    revised bmi calc using PEMDAS method

--- bmi-calculator.py
+++ bmi-calculator.py
@@ -1,5 +1,14 @@
-height = input("enter your height in m: ")
-weight = input("enter your weight in kg: ")
-bmi = float(weight)/float(height) ** 2
+# height = input("enter your height in m: ")
+# weight = input("enter your weight in kg: ")
+# bmi = float(weight)/float(height) ** 2
+# bmi_as_int = int(bmi)
+# print(bmi_as_int)
+
+# version 3 more sophisticated!
+
+height = input("enter your height in meters")
+weight = input("enter your weight in kilo's")
+
+bmi = weight_as_int / (height_as_float * height_as_float) 
 bmi_as_int = int(bmi)
 print(bmi_as_int)
