c4e7acccade347b3878e1f58e584ff8edae421f3
Benjamin144 <josephroberts@email.com>
2022-02-08 22:35:55 +0000

    revised bmi output as integer

--- bmi-calculator.py
+++ bmi-calculator.py
@@ -1,4 +1,5 @@
 height = input("enter your height in m: ")
 weight = input("enter your weight in kg: ")
 bmi = float(weight)/float(height) ** 2
-print(round(bmi))
+bmi_as_int = int(bmi)
+print(bmi_as_int)
