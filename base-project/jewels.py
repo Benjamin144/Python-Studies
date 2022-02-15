bb25e62733702da8cd7c8d726e6457e3482f2d2b
Benjamin144 <josephroberts@email.com>
2022-02-15 18:13:14 +0000

    initial commit

--- base-project/jewels.py
+++
@@ -1,7 +0,0 @@
-print("**************************************")
-password = input("Enter password: ")
-if password == "qwerty":
-    print("Correct password!")
-else:
-    print("Incorrect password")
-print("**************************************")
---
+++ tip-calculator.py
@@ -0,0 +1,6 @@
+print = ("Welcome to the tip calculator!" )
+total_bill_as_int = input("What was the total bill? ")
+percentage_as_int = input("What percentage would you like to give? ")
+split_as_int = input("How many people to split the bill? ")
+
+#print(f"Each person should pay?{})
