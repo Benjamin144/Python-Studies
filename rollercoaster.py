44a1cb1f27b2c01915bfd6c47e61d5694eb132c0
Benjamin144 <josephroberts@email.com>
2022-03-01 20:57:27 +0000

    notes for conditional operators

--- rollercoaster.py
+++ rollercoaster.py
@@ -5,3 +5,10 @@ if height >= 120:
     print("You can ride the rollercoaster!")
 else:
     print("Sorry, you have to grow taller before you can ride.")
+
+# additional conditional operators
+
+# == Equal to
+# != Not equal to
+# >= Greater than or equal to
+# <= Less than or equal to
