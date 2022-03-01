d5cd17f8b3ea2ec92c6450d855dff9e2aa25ffa3
Benjamin144 <josephroberts@email.com>
2022-03-01 20:50:50 +0000

    Rollercoaster app completion

--- rollercoaster.py
+++ rollercoaster.py
@@ -1,6 +1,7 @@
 print("Welcome to the rollercoaster!")
-height = int(height("What is your height in cm? "))
+height = int(input("What is your height in cm? "))
 
-if height > 120:
-    print("You can ride the rolercoaster!")
-else
+if height >= 120:
+    print("You can ride the rollercoaster!")
+else:
+    print("Sorry, you have to grow taller before you can ride.")
