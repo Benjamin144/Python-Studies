ebc0349b8495c1eef80497933ddb0350fa9963b8
Benjamin144 <josephroberts@email.com>
2022-03-02 21:55:52 +0000

    added nested conditions for pricing

--- rollercoaster.py
+++ rollercoaster.py
@@ -3,6 +3,14 @@ height = int(input("What is your height in cm? "))
 
 if height >= 120:
     print("You can ride the rollercoaster!")
+    age = int(input("What is your age?"))
+    if age < 12: # if they are under 12 then....
+        print("Please pay £5.")
+    elif age <= 18: # are they over 18 then? elif (else if could you also check that the following condition is true)
+        print("Please pay £7.")
+    else: # else if they are not under 12 and over 18 then the following condtion is true
+        print("Please pay £12.")
+    
 else:
     print("Sorry, you have to grow taller before you can ride.")
 
