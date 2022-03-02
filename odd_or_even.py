155eb9594a429c4fc9eecf79645176939d6e2721
Benjamin144 <josephroberts@email.com>
2022-03-02 05:12:49 +0000

    initial commit

---
+++ odd_or_even.py
@@ -0,0 +1,8 @@
+number = int(input("Which number do you want to check? "))
+
+if (number % 2) == 0:
+    print("This is an Even number.". format(number))
+else:
+    print("{0}This is an Odd number.". format(number))
+
+# Alternativley displayed using placeholders for the value of 0 (as shown in the else condition)
