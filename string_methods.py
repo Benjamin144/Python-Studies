e9b854c526295ab9b57fecd45d8686fd4f0eb826
Benjamin144 <josephroberts@email.com>
2022-03-02 05:44:03 +0000

    initial commit

---
+++ string_methods.py
@@ -0,0 +1,22 @@
+my_string = "HELLO WORLD"
+my_string_lower_case = my_string.lower()
+
+print(my_string_lower_case)
+
+my_string_2 = "hElLo WorLD"
+my_string_2_upper_case = my_string_2.upper()
+
+print (my_string_2_upper_case)
+print (my_string.isalpha())
+
+greetings = my_string.replace("WORLD", "Dave")
+print(greetings)
+
+motion = ("jump", "walk", "run")
+new_string = "ing ".join(motion)
+print(new_string)
+
+print(my_string_2.split(" "))
+
+spaced_string = " 42 "
+print(spaced_string.strip())
