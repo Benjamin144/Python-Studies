d64ff4c110254b4282085514221c11ab471209bd
Benjamin144 <josephroberts@email.com>
2022-03-03 20:40:32 +0000

    Initial commit

---
+++ fizz_buzz.py
@@ -0,0 +1,19 @@
+class Example(object):  
+   def Fizz_Buzz(self, n):  
+      """  
+      :type n: int  
+      :rtype: List[str]  
+      """  
+      result = []  
+      for i in range(1 , n+1):  
+         if i % 3 == 0 and i % 5 == 0:  
+            result.append(" Fizz_Buzz ")  
+         elif i % 3 == 0:  
+            result.append(" Fizz ")  
+         elif i % 5 == 0:  
+            result.append(" Buzz ")  
+         else:  
+            result.append(str(i))  
+      return result  
+object1 = Example()  
+print(object1.Fizz_Buzz(55)) 
