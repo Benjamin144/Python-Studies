d6c9c9ddaad49af4b1906a2fecffab1d7f864bdb
Benjamin144 <josephroberts@email.com>
2022-03-07 22:58:07 +0000

    revisions to conditionals

--- bmi_calculator_2.0.py
+++ bmi_calculator_2.0.py
@@ -3,14 +3,17 @@ weight = float(input("enter your weight in kg: "))
 bmi = float(weight)/float(height) ** 2
 bmi_as_int = int(bmi)
 
-if bmi_as_int <= 18:
+bmi = float(weight)/float(height) ** 2
+bmi_as_int = int(bmi)
+
+if bmi_as_int <= 18.5:
     print(f"Your BMI is {bmi_as_int}, you are underweight.")
-if bmi_as_int <= 22:
-    print(f"Your BMI is {bmi_as_int}, you have a normal weight.")
-if bmi_as_int <= 28:
-    print(f"Your BMI is {bmi_as_int}, you are slightly overweight")
-if bmi_as_int <= 33:
-    print(f"Your BMI is {bmi_as_int}, you are obese.")
+    if bmi_as_int > 18.5 < 25:
+        print(f"Your BMI is {bmi_as_int}, you have a normal weight.")
+    elif bmi_as_int > 25  < 30: 
+        print(f"Your BMI is {bmi_as_int}, you are slightly overweight")
+    elif bmi_as_int > 30 < 35:
+        print(f"Your BMI is {bmi_as_int}, you are obese.")
 else:
     print(f"Your BMI is {bmi_as_int} you are clinically obese.")
 
