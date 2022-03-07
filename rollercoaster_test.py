d53cb0987d14611e83eda7edba14a5e6c0495cc5
Benjamin144 <josephroberts@email.com>
2022-03-07 20:50:38 +0000

    further tests & variations to Else/Elif statements

--- rollercoaster_test.py
+++ rollercoaster_test.py
@@ -1,20 +1,22 @@
-print("Welcome to the rollercoaster!")
+print("Welcome to the Rollercoaster!")
 height = int(input("What is your height in cm? "))
 
 if height >= 120:
     print("You can ride the rollercoaster!")
-    age = int(input("What is your age?"))
+    age = int(input("What is your age? "))
     if age < 12: # if they are under 12 then....
         print("Please pay £5.")
-    elif age <= 18: # are they over 18 then? elif (else if could you also check that the following condition is true)
+    elif age <= 17: # are they over 18 then? elif (else if could you also check that the following condition is true)
         print("Please pay £7.")
+    elif age <= 19:
+        print("Please pay £9.")
     else: # else if they are not under 12 and over 18 then the following condtion is true
         print("Please pay £12.")
     
 else:
     print("Sorry, you have to grow taller before you can ride.")
 
-# additional conditional operators
+# Wprking with conditional operators 
 
 # == Equal to
 # != Not equal to
