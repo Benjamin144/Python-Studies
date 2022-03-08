0f3e38546a3be531e3d6e0339e9c4bd0446a3976
Benjamin144 <josephroberts@email.com>
2022-03-08 08:44:51 +0000

    working out conditional operators for BMI Calc 2.0

--- bmi_calculator_2.0.py
+++ bmi_calculator_2.0.py
@@ -1,19 +1,17 @@
 height = float(input("enter your height in m: "))
 weight = float(input("enter your weight in kg: "))
 bmi = float(weight)/float(height) ** 2
+bmi_guage = 35
+bmi_guage_int = int(bmi_guage)
 bmi_as_int = int(bmi)
-
-bmi = float(weight)/float(height) ** 2
-bmi_as_int = int(bmi)
-
-if bmi_as_int <= 18.5:
-    print(f"Your BMI is {bmi_as_int}, you are underweight.")
-    if bmi_as_int > 18.5 < 25:
-        print(f"Your BMI is {bmi_as_int}, you have a normal weight.")
-    elif bmi_as_int > 25  < 30: 
-        print(f"Your BMI is {bmi_as_int}, you are slightly overweight")
-    elif bmi_as_int > 30 < 35:
-        print(f"Your BMI is {bmi_as_int}, you are obese.")
+if bmi_guage >= 18.5:
+    print(f"Your BMI is {bmi_as_int}, you are slightly overweight.")
+    # if bmi_as_int < 18.5:
+    #     print(f"Your BMI is {bmi_as_int}, you have a normal weight.")
+    # elif bmi_as_int > 25: 
+    #     print(f"Your BMI is {bmi_as_int}, you are slightly overweight")
+    # elif bmi_as_int > 30:
+    #     print(f"Your BMI is {bmi_as_int}, you are obese.")
 else:
     print(f"Your BMI is {bmi_as_int} you are clinically obese.")
 
