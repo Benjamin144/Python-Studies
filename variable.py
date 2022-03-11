b997f27c9ea4edd07988f709df3ec2f970b2b731
Benjamin144 <josephroberts@email.com>
2022-03-11 08:48:09 +0000

    understanding expressions & operands usefulness in this code.

--- variable.py
+++ variable.py
@@ -1,9 +1,12 @@
 a = input("a: ")
 b = input("b: ")
 
-c = a # The same value that is input "a" is now referenced in "c" and also stored there in "c"
-a = b # The value that is "a" is now the value of "b"
-b = c # Now "b" has now become the value stored in "c"
+c = a # The value of 'a'condition that is an input "a", was changed and referenced in "c" and also stored in "c"
+a = b # The value that was stored in "a" is now the value of "b"
+b = c # Now the value "b" has replaced the value stored in 'c' and has become the 'new face of' the value stored in "c"
+
+# Therefore the original state of each value has changed positionally and condiionally
+# Also remember that when reading expressions in python conider reading them from right to left to understand the meaning of the operand
 
 print("a: " + a)
 print("b: " + b)
