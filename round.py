69c9420f8caa18feb4c469a82d78a7b84195432b
Benjamin144 <josephroberts@email.com>
2022-03-12 17:25:48 +0000

    initial commit

---
+++ round.py
@@ -0,0 +1,5 @@
+# round a number
+number = float(input("input any number?\n"))
+rounded__number = round(number)
+message = (f"your number rounded to the nearest decimal place would be {rounded__number}")
+print(message)
