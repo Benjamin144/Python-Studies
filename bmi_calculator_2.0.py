7488c35235b112ba0cddf9528131e0a634f0dfc7
Benjamin144 <josephroberts@email.com>
2022-03-13 21:37:07 +0000

    python if else elif revision

--- bmi_calculator_2.0.py
+++ bmi_calculator_2.0.py
@@ -1,19 +1,36 @@
 height = float(input("enter your height in m: "))
 weight = float(input("enter your weight in kg: "))
-bmi = float(weight)/float(height) ** 2
-bmi_guage = 35
-bmi_guage_int = int(bmi_guage)
+
+bmi = (weight)/(height) ** 2
 bmi_as_int = int(bmi)
-if bmi_guage >= 18.5:
-    print(f"Your BMI is {bmi_as_int}, you are slightly overweight.")
-    # if bmi_as_int < 18.5:
-    #     print(f"Your BMI is {bmi_as_int}, you have a normal weight.")
-    # elif bmi_as_int > 25: 
-    #     print(f"Your BMI is {bmi_as_int}, you are slightly overweight")
-    # elif bmi_as_int > 30:
-    #     print(f"Your BMI is {bmi_as_int}, you are obese.")
+your_bmi = 18
+
+if bmi_as_int < 35:
+    if your_bmi > 18.5: 
+        print(f"Your BMI is {your_bmi}, you are underweight.")
+    # print(f"Your BMI is {bmi_as_int}, you are slightly overweight.")
+    # print(f"Your BMI is {bmi_as_int}, you are slightly overweight.")
+    # print(f"Your BMI is {bmi_as_int}, you are slightly overweight.")
 else:
-    print(f"Your BMI is {bmi_as_int} you are clinically obese.")
+    print(f"Your BMI is {your_bmi} you are clinically obese.")
+
+
+
+
+
+# bmi_guage = 35
+# bmi_guage_int = int(bmi_guage)
+# bmi_as_int = int(bmi)
+# if bmi_guage >= 18.5:
+#     print(f"Your BMI is {bmi_as_int}, you are slightly overweight.")
+#     # if bmi_as_int < 18.5:
+#     #     print(f"Your BMI is {bmi_as_int}, you have a normal weight.")
+#     # elif bmi_as_int > 25: 
+#     #     print(f"Your BMI is {bmi_as_int}, you are slightly overweight")
+#     # elif bmi_as_int > 30:
+#     #     print(f"Your BMI is {bmi_as_int}, you are obese.")
+# else:
+#     print(f"Your BMI is {bmi_as_int} you are clinically obese.")
 
 
 
