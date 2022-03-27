7eff0c10d951f2e24d43f1abd6c08b5cfddbc22a
Benjamin144 <josephroberts@email.com>
2022-03-27 22:31:05 +0000

    working on logic for bmi calculations

--- bmi_calculator_2.0.py
+++ bmi_calculator_2.0.py
@@ -15,6 +15,30 @@ else:
     print(f"Your BMI is {your_bmi} you are clinically obese.")
 
 
+height = float(input("enter your height in m: "))
+weight = float(input("enter your weight in kg: "))
+
+def calculate_bmi():
+    kg = int(weight_tf.get())
+    m = int(height_tf.get())/100
+    bmi = kg/(m*m)
+    bmi = round(bmi, 1)
+    bmi_index(bmi)
+
+def bmi_index(bmi):
+    
+    if bmi < 18.5:
+        messagebox.showinfo('bmi-pythonguides', f'BMI = {bmi} is Underweight')
+    elif (bmi > 18.5) and (bmi < 24.9):
+        messagebox.showinfo('bmi-pythonguides', f'BMI = {bmi} is Normal')
+    elif (bmi > 24.9) and (bmi < 29.9):
+        messagebox.showinfo('bmi-pythonguides', f'BMI = {bmi} is Overweight')
+    elif (bmi > 29.9):
+        messagebox.showinfo('bmi-pythonguides', f'BMI = {bmi} is Obesity') 
+    else:
+        messagebox.showerror('bmi-pythonguides', 'something went wrong!')  
+
+
 
 
 
