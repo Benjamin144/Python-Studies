74152f39767ec79aa081c86245c6bbb4ea0a8c90
Benjamin144 <josephroberts@email.com>
2022-04-08 18:57:23 +0000

    revised elif statements

--- rollercoaster_test.py
+++ rollercoaster_test.py
@@ -4,19 +4,24 @@ height = int(input("What is your height in cm? "))
 if height >= 120:
     print("You can ride the rollercoaster!")
     age = int(input("What is your age? "))
-    if age < 12: # if they are under 12 then....
+    if age < 12:  # if they are under 12 then....
         print("Please pay £5.")
-    elif age <= 17: # are they over 18 then? elif (else if could you also check that the following condition is true)
+    # are they over 18 then? elif (else if could you also check that the following condition is true)
+    elif age <= 17:
         print("Please pay £7.")
     elif age <= 19:
         print("Please pay £9.")
-    else: # else if they are not under 12 and over 18 then the following condtion is true
-        print("Please pay £12.")
-    
+    elif age <= 22:
+        print("PLease pay £12")
+    elif age <= 35:
+        print("Please pay £15")
+    else:  # else if they are not under 12 and over 18 then the following condtion is true
+        print("Please pay £20.")
+
 else:
     print("Sorry, you have to grow taller before you can ride.")
 
-# Wprking with conditional operators 
+# Wprking with conditional operators
 
 # == Equal to
 # != Not equal to
@@ -29,4 +34,3 @@ else:
 # **= Exponent AND
 # %= Modulo AND
 # //= Floor Division AND
-
