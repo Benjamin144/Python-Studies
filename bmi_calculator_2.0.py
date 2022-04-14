b8d2a97fb54a3747bcd7f28e2ba610449fce2eb3
Benjamin144 <josephroberts@email.com>
2022-04-14 23:13:34 +0000

    refactored and added round function to bmi formula to display correct results

--- bmi_calculator_2.0.py
+++ bmi_calculator_2.0.py
@@ -1,34 +1,15 @@
 height = float(input("enter your height in m: "))
 weight = float(input("enter your weight in kg: "))
 
-bmi = (weight)/(height) ** 2
-bmi_as_int = int(round(bmi, 2))
+bmi = round(weight/height ** 2)
 
-if bmi_as_int < 35:
-    if bmi_as_int <= 18.5:
-        print(f"Your BMI is {bmi_as_int}, you are \033[1munderweight")
-    elif bmi_as_int <= 25:
-        print(f"Your BMI is {bmi_as_int}, you are \033[1mnormal weight")
-    elif bmi_as_int <= 30:
-        print(f"Your BMI is {bmi_as_int}, you are \033[1moverweight")
-    elif bmi_as_int <= 35:
-        print(f"Your BMI is {bmi_as_int}, you are \033[1mobese")
+if bmi <= 18.5:
+    print(f"Your BMI is {bmi}, you are \033[1munderweight.")
+elif bmi <= 25:
+    print(f"Your BMI is {bmi}, you have a \033[1mnormal weight.")
+elif bmi <= 30:
+    print(f"Your BMI is {bmi}, you are \033[1mslightly overweight.")
+elif bmi <= 35:
+    print(f"Your BMI is {bmi}, you are \033[1mobese.")
 else:
-    print(f"Your BMI is {bmi_as_int} you are \033[1mclinically obese.")
-
-    # total_usd = round(euro * usd, 2)
-
-#     bmi = (weight)/(height) ** 2
-# bmi_as_int = int(round(bmi, 2))
-
-# if bmi_as_int < 35:
-#     if bmi_as_int <= 18.5:
-#         print(f"Your BMI is {bmi_as_int}, you are \033[1munderweight")
-#     elif bmi_as_int <= 25:
-#         print(f"Your BMI is {bmi_as_int}, you are \033[1mnormal weight")
-#     elif bmi_as_int <= 30:
-#         print(f"Your BMI is {bmi_as_int}, you are \033[1moverweight")
-#     elif bmi_as_int <= 35:
-#         print(f"Your BMI is {bmi_as_int}, you are \033[1mobese")
-# else:
-#     print(f"Your BMI is {bmi_as_int} you are \033[1mclinically obese.")
+    print(f"Your BMI is {bmi} you are \033[1mclinically obese.")
