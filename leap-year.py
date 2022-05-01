c3963c64dcd6b15d239cb9586267007efa56470e
Joseph Roberts <jojumbotron@gmail.com>
2022-05-01 19:13:36 +0100

    solving leap year challenge

---
+++ leap-year.py
@@ -0,0 +1,8 @@
+leapYear = 2000/4
+if leapYear / 100:
+    print("This is not a leap year.")
+elif leapYear % 100 == 0:
+    print("This is not a leap year")
+else:
+    print("This is a leap year")
+print(leapYear)
