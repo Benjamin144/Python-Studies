cca4096a3708686bf918a72dbf689168746a029c
Benjamin144 <josephroberts@email.com>
2022-06-06 20:51:30 +0000

    coded various leap year solutions

--- leap-year-beginner_v2.py
+++ leap-year-beginner_v2.py
@@ -1,7 +1,7 @@
 print('Hello Folks, Lets find out if an Earth year is a leap year....\n')
 print('Now lets begin....')
 year =  int(input("Enter any Earth year please:\n"))
-if year % 4 == 0 and year % 100 != 0:
+if year % 4 == 0 and year % 100 != 0: # you need to test the mdulo operator to define if ==0 is true
     leap_year = True
     print("This is a leap year")
 elif year % 400 == 0 and year % 100 == 0:
--- leap-year.py
+++ leap-year.py
@@ -6,7 +6,12 @@ def is_a_leapyear(year):
     # Logic
     if year%400 == 0:
         leap = true
-    elif year%4 == 0 and year%100 !=0:
+    elif year%4 == 0 and year%100 !=0: # The % operator 'returns' the modulo (i.e. remainder) of 'a' division. 
+                                       # The == operator 'compares the value or equality of two objects', 
+                                       # (a%b==0) is true when a is divisible by b.
+                                       # They are testing if the year is divisible by 4. 
+                                       # My guess is that they are testing if it is a leap year. 
+                                       # However you'd also need to add a test to see if the year is also dividible by 100 or 400.
         leap = True
     return leap
 
