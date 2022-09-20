2b2035afe0eefb5a1da3e4dd520a0b54b7a99a28
Benjamin144 <josephroberts@email.com>
2022-09-20 03:26:13 +0000

    update to list code

---
+++ append-list.py
@@ -0,0 +1,10 @@
+numbers = []
+# strings = []
+names = ['John', 'Eric', 'Jessica']
+numbers.append(1)
+numbers.append(2)
+numbers.append(3)
+second_name = names.append(1)
+print(numbers)
+# print(strings)
+print('The second name on the names list %s' % second_name)
