3dc36af54e3d1adaa811fe11a7832a1c369e708c
Benjamin144 <josephroberts@email.com>
2022-09-20 20:14:06 +0000

    completed code for listing listing stringd and integers

--- append-list.py
+++ append-list.py
@@ -1,10 +1,12 @@
 numbers = []
-# strings = []
+strings = []
 names = ['John', 'Eric', 'Jessica']
 numbers.append(1)
 numbers.append(2)
 numbers.append(3)
-second_name = names.append(1)
+strings.append('Hello')
+strings.append('World')
+second_name = names[1]
 print(numbers)
-# print(strings)
+print(strings)
 print('The second name on the names list %s' % second_name)
