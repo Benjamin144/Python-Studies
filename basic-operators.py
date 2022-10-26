595e11de54896f091e2c02f5f363d366abf27c9f
Benjamin144 <josephroberts@email.com>
2022-10-26 14:58:30 +0000

    file and code for small math operations

---
+++ basic-operators.py
@@ -0,0 +1,19 @@
+# remainder = 11 % 3
+# print(remainder)
+
+# The answer will be 2 since 3 goes into 11 3 times leaving 2 remainding
+
+number = 1 + 2 * 3 / 4.0
+print(number)
+
+num1 = 1 + 2
+print(num1)
+
+num2 = 3 * 3
+print(num2)
+
+num3 = 9 / 4.0
+print(num3)
+
+num4 = 0.25 * 4
+print(num4)
---
+++ basic_multi.py
@@ -0,0 +1,17 @@
+squared = 7 ** 2  # the root squared
+squaredActual = 7 * 7
+print(squared)
+print(squaredActual)
+
+cuded = 2 ** 3  # to the power of 3 (operation broken down below)
+print(cuded)
+
+cubed1 = 2 * 2 * 2
+print(cubed1)
+# ...and biquadrates or tesseractic numbers which are fourth power sequences
+
+biquad = 2 ** 4
+print(biquad)
+
+biquad1 = 4 ** 4
+print(biquad1)
