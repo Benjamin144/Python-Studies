b1f18d26ba8c654faf4f36998ac37075343f258a
Benjamin144 <josephroberts@email.com>
2022-10-28 14:12:40 +0000

    code snippets for list sequence multiplier

---
+++ list-operators.py
@@ -0,0 +1,7 @@
+even_numbers = [2, 4, 6, 8, 10]
+odd_numbers = [1, 3, 5, 7, 9, ]
+all_numbers = odd_numbers + even_numbers
+print(all_numbers)
+
+# repeating sequences on one row in series
+print([1, 2, 3, 4, 5] * 5)
