7281281faa9d75e4d4a6b8424c6a71fb16e0b78e
Benjamin144 <josephroberts@email.com>
2022-10-29 17:13:40 +0000

    code snippet for using the %s argument specifier

---
+++ string-formatting.py
@@ -0,0 +1,7 @@
+# This prints out "Hello John!"
+# print("Hello, %s!" % name)
+# newName = "Eleth"
+# print("My name is %s" % newName)
+
+architecture = "The Empire State is"
+print("%s a famous American building" % architecture)
