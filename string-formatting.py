b1518a774d62430ca0e705c53b576d4cd616fedc
Benjamin144 <josephroberts@email.com>
2022-10-29 17:45:56 +0000

    using two argument specifiers to make a sentance

--- string-formatting.py
+++ string-formatting.py
@@ -1,7 +1,20 @@
 # This prints out "Hello John!"
+# name = John
 # print("Hello, %s!" % name)
 # newName = "Eleth"
 # print("My name is %s" % newName)
 
-architecture = "The Empire State is"
-print("%s a famous American building" % architecture)
+# architecture = "The Empire State is"
+# print("%s a famous American building" % architecture)
+
+
+# This prints out "John is 23 years old."
+
+# name = "John"
+# age = 23
+# print("%s is %d years old" % (name, age))
+
+car = "Volkswagen"
+period = 1920
+
+print("I have an amazing %s from %s for sale" % (car, period))
