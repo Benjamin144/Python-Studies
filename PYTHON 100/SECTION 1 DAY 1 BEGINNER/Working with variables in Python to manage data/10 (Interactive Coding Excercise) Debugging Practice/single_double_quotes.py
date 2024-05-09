41b4a12bd273063ed4e77037f2b048e00d3e0dbb
Benjamin144 <josephroberts@email.com>
2024-05-09 19:17:56 +0000

    added code to understand quotation marks syntax

---
+++ PYTHON 100/SECTION 1 DAY 1 BEGINNER/Working with variables in Python to manage data/10 (Interactive Coding Excercise) Debugging Practice/single_double_quotes.py
@@ -0,0 +1,3 @@
+print("'a single quote' inside a double quote")
+print('"a double quotes" inside a single quote') # string literals can become unterminated if incorrect quotes are used
+print("alternativley, you can just \'escape\' the quote")
